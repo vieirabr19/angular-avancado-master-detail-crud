3adb3c05527af83aa5fee6c41c05bdd4adb5fa3c
Luiz Vieira <luiz22_sp@hotmail.com>
2020-04-26 13:55:31 -0300

    Adding in-memory-database for categories

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
 const routes: Routes = [
   {path: 'categories', loadChildren: () => import('./pages/categories/categories.module').then(m => m.CategoriesModule) },
   {path: 'reports', loadChildren: () => import('./pages/reports/reports.module').then(m => m.ReportsModule) }
