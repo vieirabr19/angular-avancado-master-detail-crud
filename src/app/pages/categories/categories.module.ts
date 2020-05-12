7a44e23ccab08efe6232aa44f54f09c8861b3bc8
Luiz Vieira <luiz22_sp@hotmail.com>
2020-05-11 22:44:22 -0300

    project restructuring

--- src/app/pages/categories/categories.module.ts
+++ src/app/pages/categories/categories.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule } from "@angular/forms";
+
+import { SharedModule } from "../../shared/shared.module";
 
 import { CategoriesRoutingModule } from './categories-routing.module';
 
@@ -14,9 +14,8 @@ import { CategoryFormComponent } from './category-form/category-form.component';
     CategoryFormComponent
   ],
   imports: [
-    CommonModule,
     CategoriesRoutingModule,
-    ReactiveFormsModule
+    SharedModule
   ]
 })
 export class CategoriesModule { }
--- src/app/pages/entries/entries.module.ts
+++ src/app/pages/entries/entries.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule } from "@angular/forms";
+
+import { SharedModule } from "../../shared/shared.module";
 
 import { CalendarModule } from "primeng/calendar";
 import { IMaskModule } from "angular-imask";
@@ -17,9 +17,8 @@ import { EntryFormComponent } from "./entry-form/entry-form.component";
     EntryFormComponent
   ],
   imports: [
-    CommonModule,
+    SharedModule,
     EntriesRoutingModule,
-    ReactiveFormsModule,
     CalendarModule,
     IMaskModule
   ]
