875cee499204eff088f8ad30b994c72fee03aa83
Luiz Vieira <luiz22_sp@hotmail.com>
2020-05-13 22:51:06 -0300

    improving BaseResourceService && refactoting CategoryService

--- src/app/pages/categories/shared/category.service.ts
+++ src/app/pages/categories/shared/category.service.ts
@@ -1,76 +1,15 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable, Injector } from '@angular/core';
 
-import { Observable, throwError } from 'rxjs';
-import { map, catchError, flatMap } from 'rxjs/operators';
-
+import { BaseResourceService } from "../../../shared/services/base-resource.service";
 import { Category } from './category.model';
 
 @Injectable({
   providedIn: 'root'
 })
-export class CategoryService {
-  private apiPath: string = "api/categories";
+export class CategoryService extends BaseResourceService<Category> {
 
-  constructor(
-    private http: HttpClient
-  ) { }
-
-  getAll(): Observable<Category[]> {
-    return this.http.get<Category[]>(this.apiPath).pipe(
-      map(this.jsonDataToCategories),
-      catchError(this.handleError)
-    )
+  constructor( protected injector: Injector ) {
+    super("api/categories", injector);
   }
-
-  getById(id: number): Observable<Category>{
-    const url: string = `${this.apiPath}/${id}`;
-    return this.http.get(url).pipe(
-      map(this.jsonDataToCategory),
-      catchError(this.handleError)
-    )
-  }
-
-  cerate(category: Category): Observable<Category>{
-    return this.http.post(this.apiPath, category).pipe(
-      map(this.jsonDataToCategory),
-      catchError(this.handleError)
-    )
-  }
-
-  update(category: Category): Observable<Category>{
-    const url: string = `${this.apiPath}/${category.id}`;
-    return this.http.put(url, category).pipe(
-      map(() => category),
-      catchError(this.handleError)
-    )
-  }
-
-  delete(id: number): Observable<any>{
-    const url: string = `${this.apiPath}/${id}`;
-    return this.http.delete(url).pipe(
-      map(() => null),
-      catchError(this.handleError)
-    )
-  }
-
-  // PRIVATE
-
-  // Imprime um array de categorias
-  private jsonDataToCategories(jsonData: any[]): Category[] {
-    const categories: Category[] = [];
-    jsonData.forEach(element => categories.push(element as Category));
-    return categories;
-  }
-
-  // Imprime uma categoria por ID
-  private jsonDataToCategory(jsonData: any): Category{
-    return jsonData as Category;
-  }
-
-  // Erroa na requisição
-  private handleError(error: any): Observable<any> {
-    console.log('ERRO NA REQUISIÇÃO =>', error);
-    return throwError(error);
-  }
+  
 }
---
+++ src/app/shared/services/base-resource.service.ts
@@ -0,0 +1,75 @@
+import { Injector } from "@angular/core";
+import { HttpClient } from '@angular/common/http';
+
+import { Observable, throwError } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
+
+import { BaseResourceModel } from "../models/base-resource.model";
+
+export abstract class BaseResourceService<T extends BaseResourceModel>{
+
+  protected http: HttpClient;
+
+  constructor( protected apiPath: string, protected injector: Injector ){
+    this.http = injector.get(HttpClient);
+  }
+
+  getAll(): Observable<T[]> {
+    return this.http.get(this.apiPath).pipe(
+      map(this.jsonDataToResources),
+      catchError(this.handleError)
+    )
+  }
+
+  getById(id: number): Observable<T>{
+    const url: string = `${this.apiPath}/${id}`;
+    return this.http.get(url).pipe(
+      map(this.jsonDataToResource),
+      catchError(this.handleError)
+    )
+  }
+
+  cerate(resource: T): Observable<T>{
+    return this.http.post(this.apiPath, resource).pipe(
+      map(this.jsonDataToResource),
+      catchError(this.handleError)
+    )
+  }
+
+  update(resource: T): Observable<T>{
+    const url: string = `${this.apiPath}/${resource.id}`;
+    return this.http.put(url, resource).pipe(
+      map(() => resource),
+      catchError(this.handleError)
+    )
+  }
+
+  delete(id: number): Observable<any>{
+    const url: string = `${this.apiPath}/${id}`;
+    return this.http.delete(url).pipe(
+      map(() => null),
+      catchError(this.handleError)
+    )
+  }
+
+  // PROTECTED
+
+  // Imprime um array de categorias
+  protected jsonDataToResources(jsonData: any[]): T[] {
+    const resources: T[] = [];
+    jsonData.forEach(element => resources.push(element as T));
+    return resources;
+  }
+
+  // Imprime uma categoria por ID
+  protected jsonDataToResource(jsonData: any): T{
+    return jsonData as T;
+  }
+
+  // Erroa na requisição
+  protected handleError(error: any): Observable<any> {
+    console.log('ERRO NA REQUISIÇÃO =>', error);
+    return throwError(error);
+  }
+
+}
