8b89e22be42472c8af5b5b7c0f530eac8cfab6f6
Luiz Vieira <luiz22_sp@hotmail.com>
2020-06-18 17:53:01 -0300

    adding reports template

--- src/app/pages/reports/reports.module.ts
+++ src/app/pages/reports/reports.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+
+import { SharedModule } from "../../shared/shared.module";
 
 import { ReportsRoutingModule } from './reports-routing.module';
 import { ReportsComponent } from './reports/reports.component';
@@ -8,7 +9,7 @@ import { ReportsComponent } from './reports/reports.component';
 @NgModule({
   declarations: [ReportsComponent],
   imports: [
-    CommonModule,
+    SharedModule,
     ReportsRoutingModule
   ]
 })
